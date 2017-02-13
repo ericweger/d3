a9a9cdf63a815dd2408ad445ebf684bd6caa0f22
Eric Weger <ericweger@gmail.com>
2017-02-12 20:20:23 -0800

    Early data display, initial bar chart

--- census.js
+++ census.js
@@ -1,11 +1,16 @@
-//variables
+//Variables
 var population = "B01003_001E";
 var income = "B19013_001E";
 var median_gross_rent = "B25064_001E";
 
+//Filter Variables
+var minPop;
+var maxPop;
+
 $(function initialize() {
     sdk = new CitySdk();
     census = new CensusModule("3a8e2ee9ffcaf4931c4966b551c551cfa48809d8");
+    cityCall();
 });
 
 function cityCall() {
@@ -26,12 +31,54 @@ function cityCall() {
 }
 var callback = function callback(response) {
 
-	let data = response["data"].filter(popFilter);
-	var rows = d3.select("#rawdata").selectAll("p").data(data).text(function(d,i) {return d["NAME"]});
-	rows.enter().append("p").text(function(d,i) {return d["NAME"] + " | Population: " + d[population] + ", Avg. Income: $" + d[income] + "/year, Median Gross Rent: $" + d[median_gross_rent] + "/month"});
-	rows.exit().remove();
+	fullData = response["data"];
+    update();
+};
+
+function update(field, value) {
+
+    data = fullData.filter(popFilter);
+    var rows = d3.select("#rawdata").selectAll("p").data(data).text(function(d,i) {return d["NAME"]});
+    rows.enter().append("p").text(function(d,i) {return d["NAME"] + " | Population: " + d[population] + ", Avg. Income: $" + d[income] + "/year, Median Gross Rent: $" + d[median_gross_rent] + "/month"});
+    rows.exit().remove();
+
+    
+
+    var bars = d3.select("#barchart")
+    .selectAll("rect")
+    .data(data);
+
+    var y = d3.scaleLinear()
+        .domain([0, d3.max(data.map( function(d) { return Number(d[population]); } ))])
+        .range([0, 800]);
+    console.log(data.map( function(d) { return Number(d[population]); } ));
+    console.log(d3.max(data.map( function(d) { return Number(d[population]); } )));
+
+    bars.attr("height", function(d,i) { return y(d[population]); })
+    .attr("width", 20)
+    .attr("x", function(d,i) { return 30 * i; })
+    .attr("y", function(d,i) {return 800 - y(d[population]);});
+    console.log(bars);
+    bars.enter()
+    .append("rect")
+    .attr("height", function(d,i) { return y(d[population]);})
+    .attr("width", 20)
+    .attr("x", function(d,i) { return 30 * i; })
+    .attr("y", function(d,i) {return 800 - y(d[population]);});
+    console.log(bars);
+    bars.exit().remove();
 };
 
 function popFilter(place) {
-	return place[population] >= 50000;
-}
+    if (minPop) {
+        if (maxPop) {
+            return place[population] >= minPop && place[population] <= maxPop;
+        } else {
+            return place[population] >= minPop;
+        };
+    } else if (maxPop) {
+        return place[population] <= maxPop;
+    } else {
+        return true;
+    };
+};
